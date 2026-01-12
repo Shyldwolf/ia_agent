abc13fb51fdcee2301691faa5f3ff45a15aece41
ShyldWolf <randolfcuello@gmail.com>
2026-01-12 15:05:18 -0400

    Cambiando modo de entrada del content a prompt

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import argparse
 from dotenv import load_dotenv
 from google import genai
 
@@ -8,12 +9,23 @@ def main():
     api_key = os.environ.get("GEMINI_API_KEY")
     if not api_key:
         raise ValueError("GEMINI_API_KEY environment variable not set.")
+    
+    parser = argparse.ArgumentParser(description="Generate content using Gemini API.")
+    parser.add_argument("user_input", type=str, help="The input prompt for content generation.")
+    args = parser.parse_args()
+    prompt = args.user_input
 
     client = genai.Client(api_key=api_key)
     response = client.models.generate_content(
     model="gemini-2.5-flash",
-    contents= " Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum")
-
+    contents=prompt
+    )
+    
+    if not response.usage_metadata:
+        raise ValueError("No usage metadata found in the response.")
+    
+    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+    print("Response tokens:", response.usage_metadata.candidates_token_count)
     print("response:")
     print(response.text)
 
